947b15507013004f44ea0aa7a3b9c5f214244352
amauni-dev <ankit.mauni@marketwicks.com>
2023-07-29 17:36:06 +0530

    last commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { SocialAuthService,FacebookLoginProvider} from '@abacritt/angularx-social-login';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-google';
+  user: any;
+  loggedIn: any;
+  constructor(private authService: SocialAuthService) { }
+
+  ngOnInit() {
+    this.authService.authState.subscribe((user:any) => {
+      this.user = user;
+      this.loggedIn = (user != null);
+      console.log("this.user", this.user)
+    });
+  }
+
+  loginWithFacebook(): void {
+    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID)
+      .then((user: any) => {
+        console.log("Facebook User",user);
+        // You can now access user details like user.id, user.name, user.email, etc.
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+  signOut(): void {
+    this.authService.signOut();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { SocialLoginModule, SocialAuthServiceConfig , GoogleSigninButtonDirective, FacebookLoginProvider} from '@abacritt/angularx-social-login';
+import {GoogleLoginProvider} from '@abacritt/angularx-social-login';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,9 +11,33 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,SocialLoginModule
+    
   ],
-  providers: [],
+  providers: [{
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '229715530543-nl07apf6o19s156087gq4t80ft33rqrt.apps.googleusercontent.com'
+            )
+          },
+          {
+            id: FacebookLoginProvider.PROVIDER_ID,
+            provider: new FacebookLoginProvider(
+              '1318309215787123'
+            )
+          }
+        ],
+        onError: (err) => {
+          console.error(err);
+        }
+      } as SocialAuthServiceConfig,
+    },
+    GoogleSigninButtonDirective],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
